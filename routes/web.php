2188cc4537182837e635e38881d1132eed2f5ccf
ahmed <ahmedmohamed2042003@gmail.com>
2025-02-04 23:34:20 +0200

    refactoring routes

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class,'index'])->name('dashboard');
 
-Route::group(['prefix'=>'idea/','as'=>'ideas.'],function(){
-    
-    Route::post('',[IdeaController::class,'store'])->name('store');
-    Route::get('/{idea}',[IdeaController::class,'show'])->name('show');
-    Route::group(['middleware'=>['auth']],function(){
-        Route::get('/{idea}/edit',[IdeaController::class,'edit'])->name('edit');
-        Route::put('/{idea}',[IdeaController::class,'update'])->name('update');
-        Route::delete('/{idea}',[IdeaController::class,'destroy'])->name('destroy');
-        Route::post('/{idea}/comments',[CommentController::class,'store'])->name('comments.store');
-    });
-   
-});
+
+Route::resource('ideas',IdeaController::class)->except(['index','create','show'])->middleware('auth');
+Route::resource('ideas',IdeaController::class)->only('show');
+Route::resource('ideas.comments',CommentController::class)->only('store')->middleware('auth');
 
 
 
