38158f32e362ad7025aaafc61b3b471326b6dba0
ahmed <ahmedmohamed2042003@gmail.com>
2025-06-30 12:26:20 +0300

    FEAT: add feed to the left-sidebar

--- resources/views/Shared/left-sidebar.blade.php
+++ resources/views/Shared/left-sidebar.blade.php
@@ -6,12 +6,17 @@
                     <span>Home</span></a>
             </li>
             <li class="nav-item">
+                <a class="{{Route::is('Feed')?'text-white bg-primary rounded' : ''}} nav-link" href="{{route('Feed')}}">
+                    <span>Feed</span></a>
+            </li>
+            <li class="nav-item">
             <a class="{{Route::is('terms')?'text-white bg-primary rounded' : ''}} nav-link" href="{{route('terms')}}">
                     <span>Terms</span></a>
             </li>
         </ul>
     </div>
     <div class="card-footer text-center py-2">
-        <a class="btn btn-link btn-sm" href="{{ route('profile') }}">View Profile </a>
+        <a class="btn btn-link btn-sm" href="{{ route('lang','en') }}">en </a>
+        <a class="btn btn-link btn-sm" href="{{ route('lang','es') }}">es </a>
     </div>
 </div>
