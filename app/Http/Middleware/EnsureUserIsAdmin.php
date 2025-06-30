a8ed6838a6177cfa34bbe494678c7cc7116c907b
ahmed <ahmedmohamed2042003@gmail.com>
2025-06-30 12:03:54 +0300

    FEAT: implement is_admin middleware

---
+++ app/Http/Middleware/EnsureUserIsAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!auth()->user()->is_admin)
+        {
+            abort(403);
+        }
+        return $next($request);   // execute the next middleware and then goes through our controller basically the entire request flow
+    }
+}
