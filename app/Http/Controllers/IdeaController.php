e39e82b753132a364ee9cd6d19f734368b985a9d
ahmed <ahmedmohamed2042003@gmail.com>
2025-06-30 11:51:38 +0300

    FEAT: add policy checks for update, edit, and delete methods in IdeaController

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateIdeaRequest;
+use App\Http\Requests\UpdateIdeaRequest;
 use App\Models\Idea;
-use Auth;
-use Illuminate\Http\Request;
-
-
-
 
 
 class IdeaController extends Controller
@@ -21,47 +18,31 @@ class IdeaController extends Controller
 
     public function edit(Idea $idea)
     {
-        if(auth()->id()!== $idea->user_id)
-        {
-            abort('404');
-        }
+        $this->authorize('update', $idea);
         $editing = true;
         return view('ideas.show', compact('idea', 'editing'));
     }
 
-    public function update(Idea $idea)
+    public function update(UpdateIdeaRequest $request,Idea $idea)
     {
-        if(auth()->id()!== $idea->user_id)
-        {
-            abort('404');
-        }
-        $validated = Request()->validate(
-            [
-                'content' => 'Required|min:3|max:240'
-            ]
-        );
+        $this->authorize('update', $idea);
+        
+        $validated = $request->validated();
         $idea->update($validated);
         return redirect()->route('ideas.show', $idea->id)->with('success', 'idea Updated successfully');
         ;
 
     }
-    public function store()
+    public function store(CreateIdeaRequest $request)
     {
-        $validated = Request()->validate(
-            [
-                'content' => 'Required|min:3|max:240'
-            ]
-        );
+        $validated = $request->validated();
         $validated['user_id']=auth()->id();
         Idea::create($validated);
         return redirect()->route('dashboard')->with('success', 'idea created successfully');
     }
     public function destroy(Idea $idea)
     {
-        if(auth()->id()!== $idea->user_id)
-        {
-            abort('404');
-        }
+       $this->authorize('delete', $idea);
         $idea->delete();
         return redirect()->route('dashboard')->with('success', 'idea deleted successfully');
     }
