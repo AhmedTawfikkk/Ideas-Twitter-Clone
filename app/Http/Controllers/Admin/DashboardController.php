517706d5ff6343bbc9523097d69f856bd705137c
ahmed <ahmedmohamed2042003@gmail.com>
2025-06-30 12:03:22 +0300

    FEAT: implement admin controllers

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Idea;
+use App\Models\User;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+         $totalUsers = User::count();
+        $totalIdeas = Idea::count();
+        $totalComments = Comment::count();
+
+        return view(
+            'admin.dashboard',
+            compact('totalUsers', 'totalIdeas', 'totalComments')
+        );
+    }
+}
