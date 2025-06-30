029c777deb7d45d726be01cfa7c0cea060dc0dfd
ahmed <ahmedmohamed2042003@gmail.com>
2025-06-30 12:00:05 +0300

    FEAT: cache and share top users globally from AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\idea;
+use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +26,28 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        /*  Replaced by EnsureUserIsAdmin middleware
+        Gate::define('admin',function(User $user){
+            return $user->is_admin;
+        });
+        */
+
+        /*  Replaced by Idea Policy
+        Gate::define('idea.delete',function(User $user,idea $idea){
+            return ($user->is_admin || $user->id === $idea->user_id);
+        });
+        Gate::define('idea.edit',function(User $user,idea $idea){
+            return ($user->is_admin || $user->id === $idea->user_id);
+        });
+        */
+
+        $topUsers = Cache::remember('topUsers', 60 * 2, function () {  //checks the cache driver to see if these top users exist and if does not exist it will run the closure and get the value and put it inside the cache
+            return User::withCount('ideas')
+                ->orderBy('ideas_count', 'desc')
+                ->limit(5)->get();
+        });
+
+        View::share('topUsers', $topUsers);
     }
 }
